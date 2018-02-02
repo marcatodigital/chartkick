68229837d464f9573c92bc53fc82802c5bd6569c
Morgan Currie <morgan@marcatodigital.com>
2018-02-02 15:34:23 -0400

    begin adding support for chartjs-plugin-annotation. added a addLineAnnotation helper function when using the ChartJS adapter

--- vendor/assets/javascripts/chartkick.js
+++ vendor/assets/javascripts/chartkick.js
@@ -1019,6 +1019,7 @@
         this.name = "chartjs";
 
         var baseOptions = {
+          annotation: {annotations: []},
           maintainAspectRatio: false,
           animation: false,
           tooltips: {
@@ -1115,6 +1116,54 @@
         var setYtitle = function (options, title) {
           options.scales.yAxes[0].scaleLabel.display = true;
           options.scales.yAxes[0].scaleLabel.labelString = title;
+        };
+
+        var addLineAnnotation = function(options, title, colour, coordinate, orientation) {
+          var scaleID = orientation == 'vertical' ? 'x-axis-0' : 'y-axis-0';
+
+          var annotation = {
+            id: "line-id-"+(options.annotation.annotations.length+1), // unique ID
+            type: 'line',
+            mode: orientation, // 'horizontal', 'vertical'
+            scaleID: scaleID,
+            value: coordinate, // the spot on the X/Y axes to place the annotation
+            borderColor: colour, // HTML colour to draw the line in
+            borderWidth: 1,
+            label: {
+              // Background color of label, default below
+              backgroundColor: 'rgba(0,0,0,0.8)',
+              // Font family of text, inherits from global
+              fontFamily: "sans-serif",
+              // Font size of text, inherits from global
+              fontSize: 12,
+              // Font style of text, default below
+              fontStyle: "bold",
+              // Font color of text, default below
+              fontColor: "#fff",
+              // Padding of label to add left/right, default below
+              xPadding: 6,
+              // Padding of label to add top/bottom, default below
+              yPadding: 6,
+              // Radius of label rectangle, default below
+              cornerRadius: 6,
+              // Anchor position of label on line, can be one of: top, bottom, left, right, center. Default below.
+              position: "center",
+              // Adjustment along x-axis (left-right) of label relative to above number (can be negative)
+              // For horizontal lines positioned left or right, negative values move
+              // the label toward the edge, and positive values toward the center.
+              xAdjust: 0,
+              // Adjustment along y-axis (top-bottom) of label relative to above number (can be negative)
+              // For vertical lines positioned top or bottom, negative values move
+              // the label toward the edge, and positive values toward the center.
+              yAdjust: 0,
+              // Whether the label is enabled and should be displayed
+              enabled: true,
+              // Text to display in label - default is null
+              content: title
+            }
+          }
+
+          options.annotation.annotations.push(annotation);
         };
 
         var drawChart = function(chart, type, data, options) {
